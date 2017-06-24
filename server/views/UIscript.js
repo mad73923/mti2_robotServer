51f2df73ed747ff39734a0046e1ef3a0164607e6
Matthias Weis <matthias1.weis@web.de>
2017-06-24 12:15:20 +0200

    return to home if client number changed

--- server/views/UIscript.js
+++ server/views/UIscript.js
@@ -3,13 +3,14 @@ var dataApp = angular.module('dataApp', ['chart.js']);
 dataApp.controller('dataCtrl', function($scope, $http){
 	updateClients();
 	$scope.currentItemIndex = -1;
+	$scope.clients = [];
 
 	// socket.io
 
 	var socket = io();
 
 	socket.on('message', function(data){
-		$scope.clients = data;
+		newData(data);
 		$scope.$apply();
 	});
 
@@ -27,7 +28,15 @@ dataApp.controller('dataCtrl', function($scope, $http){
 
 	function updateClients(){
 		$http.get('/clients.json').then(function(data){
-			$scope.clients = data.data;
+			newData(data.data);
 		});
+	};
+
+	function newData(data){
+		let oldLength = $scope.clients.length;
+		$scope.clients = data;
+		if(oldLength != $scope.clients.length){
+			$scope.currentItemIndex = -1;
+		}
 	};
 });
